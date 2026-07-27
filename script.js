f83d98e07e274ffa3584e2007935f896fa7ef56e
dhananjayachamindhu100-blip <dhananjayachamindhu100@gmail.com>
2026-07-27 16:35:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-console.log("HelaBuy Loaded!");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
-const button = document.querySelector("button");
+const firebaseConfig = {
+  apiKey: "AIzaSyDl3uFTItmYbC2DYSqyleox_VSyxOLgpVk",
+  authDomain: "helabuy-2458b.firebaseapp.com",
+  projectId: "helabuy-2458b",
+  storageBucket: "helabuy-2458b.firebasestorage.app",
+  messagingSenderId: "968570968832",
+  appId: "1:968570968832:web:88377ff2989850b81f31e5",
+  measurementId: "G-GELCK9Q0DM"
+};
 
-button.addEventListener("click", () => {
-  alert("Welcome to HelaBuy! Shopping feature coming soon.");
-});
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+async function loadProducts() {
+  const querySnapshot = await getDocs(collection(db, "products"));
+
+  querySnapshot.forEach((doc) => {
+    console.log(doc.id, doc.data());
+  });
+}
+
+loadProducts();
