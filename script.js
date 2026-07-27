88c6955730eb3078f3d5b06d38a819f8dbe50725
dhananjayachamindhu100-blip <dhananjayachamindhu100@gmail.com>
2026-07-27 14:39:33 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("HelaBuy Loaded!");
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+  alert("Welcome to HelaBuy! Shopping feature coming soon.");
+});
